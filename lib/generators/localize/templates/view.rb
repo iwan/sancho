8d7bd6b049b71261e478b2e1a272ec693f413d75
Iwan Buetti <iwan.buetti@gmail.com>
2014-05-06 15:19:37 +0200

    added form label to localize generator

--- lib/generators/localize/templates/view.rb
+++ lib/generators/localize/templates/view.rb
@@ -1,13 +1,15 @@
 <%= @loc %>:
   <%= plural_name %>:
+    controller:
+      create: <%= "#{human_name} was successfully created" %>
+      update: <%= "#{human_name} was successfully updated" %>
+      destroy: <%= "#{human_name} was successfully destroyed" %>
     form:
       save: <%= "save".gootra(@loc) %>
       cancel: <%= "cancel".gootra(@loc) %>
     show:
       title: <%= "#{human_name} detail".gootra(@loc) %>
       index: <%= "All #{plural_name}".gootra(@loc) %>
-      edit:
-      destroy:
 <% @attributes.each do |attribute| -%>
       <%= attribute %>: <%= attribute.gootra(@loc) %>
 <% end -%>
@@ -19,3 +21,11 @@
       title: <%= "Edit #{file_name}".gootra(@loc) %>
     new:
       title: <%= "New #{file_name}".gootra(@loc) %>
+
+# --- Form labels:
+  helpers:
+    label:
+      <%= singular_name %>:
+<% @attributes.each do |attribute| -%>
+        <%= attribute %>: <%= attribute.gootra(@loc) %>
+<% end -%>
